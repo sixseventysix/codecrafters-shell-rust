88e95816b6d0ea934ed57360c21cd7a8b99a3af1
samarth <samarthkulkarni@icloud.com>
2025-01-09 13:59:21 +0530

    6

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,13 @@ fn main() {
         match input.trim() {
             "exit 0" => std::process::exit(0),
             input if input.starts_with("echo ") => println!("{}", &input[5..]),
+            input if input.starts_with("type ") => {
+                let arg = &input[5..];
+                match arg {
+                    "exit" | "echo" | "type" => println!("{arg} is a shell builtin"),
+                    _ => println!("{arg}: not found"),
+                }
+            }
             input => println!("{}: command not found", input),
         };
     }
