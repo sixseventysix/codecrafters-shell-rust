36f420d4a23e0b4f630103808cfceb4bf3f9233c
samarth <samarthkulkarni@icloud.com>
2025-01-09 13:46:05 +0530

    stage 4

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,12 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+
+        match input.as_str() {
+            "exit 0" => { std::process::exit(0); }
+            _ => {
+                println!("{}: command not found", input.trim());
+            }
+        }
     }
 }
