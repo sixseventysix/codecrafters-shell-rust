8ebefa83af67367769bd524dc35c52d56b81106e
samarth <samarthkulkarni@icloud.com>
2025-01-09 13:47:47 +0530

    4

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        match input.as_str() {
+        match input.as_str().trim() {
             "exit 0" => { std::process::exit(0); }
             _ => {
                 println!("{}: command not found", input.trim());
